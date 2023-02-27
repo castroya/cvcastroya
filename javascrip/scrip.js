60339615e22cef4e35822290448a3b8b08e4c973
castroya <celeste200725@gmail.com>
2023-02-27 17:11:57 -0300

    first commit

---
+++ javascrip/scrip.js
@@ -0,0 +1,40 @@
+document.getElementById("listaContacto-click1").onmouseover = function () {
+    document.getElementById("listaContacto-click1").style.color = "#262626";
+    document.getElementById("listaContacto-click1").innerHTML = "catalina_estevez@gmal.com";
+}
+document.getElementById("listaContacto-click1").onmouseout = function () {
+    document.getElementById("listaContacto-click1").style.color = "#999999";
+    document.getElementById("listaContacto-click1").innerHTML = "Email: catalina_estevez@gmail.com";
+}
+document.getElementById("listaContacto-click2").onmouseover = function () {
+    document.getElementById("listaContacto-click2").style.color = "#262626";
+    document.getElementById("listaContacto-click2").innerHTML = "(11) 722322583";
+}
+document.getElementById("listaContacto-click2").onmouseout = function () {
+    document.getElementById("listaContacto-click2").style.color = "#999999";
+    document.getElementById("listaContacto-click2").innerHTML = "Teléfono: (011) 722322583";
+}
+document.getElementById("listaContacto-click3").onmouseover = function () {
+    document.getElementById("listaContacto-click3").style.color = "#262626";
+    document.getElementById("listaContacto-click3").innerHTML = "Lanús-Buenos Aires-Argentina";
+}
+document.getElementById("listaContacto-click3").onmouseout = function () {
+    document.getElementById("listaContacto-click3").style.color = "#999999";
+    document.getElementById("listaContacto-click3").innerHTML = "Localidad: Lanús-Buenos Aires-Argentina";
+}
+document.getElementById("listaContacto-click4").onmouseover = function () {
+    document.getElementById("listaContacto-click4").style.color = "#262626";
+    document.getElementById("listaContacto-click4").innerHTML = "Arenales 4135";
+}
+document.getElementById("listaContacto-click4").onmouseout = function () {
+    document.getElementById("listaContacto-click4").style.color = "#999999";
+    document.getElementById("listaContacto-click4").innerHTML = "Dirección: Arenales 4135";
+}
+document.getElementById("listaContacto-click5").onmouseover = function () {
+    document.getElementById("listaContacto-click5").style.color = "#262626";
+    document.getElementById("listaContacto-click5").innerHTML = "1824";
+}
+document.getElementById("listaContacto-click5").onmouseout = function () {
+    document.getElementById("listaContacto-click5").style.color = "#999999";
+    document.getElementById("listaContacto-click5").innerHTML = "Código Postal: 1824";
+}
